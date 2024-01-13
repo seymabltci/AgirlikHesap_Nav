b6ff4b5e7ee5c82ea5c0b9e256565f829bbfa927
seymabltci <seymabaltaci19@gmail.com>
2024-01-13 13:01:52 +0300

    AgirlikHesap_Nav

--- app/src/main/java/com/seyma/agirlikhesap_nav/MainActivity.kt
+++ app/src/main/java/com/seyma/agirlikhesap_nav/MainActivity.kt
@@ -10,8 +10,6 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 import androidx.core.view.GravityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.seyma.agirlikhesap_nav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){ //MainActivity Sınıfı: Bu sınıf, AppCompatActivity sınıfından türemiş bir sınıf.
@@ -21,117 +19,33 @@ class MainActivity : AppCompatActivity(){ //MainActivity Sınıfı: Bu sınıf, 
                                                       // tanımlandı. Bu özellik, ViewBinding kullanılarak
                                                       // activity_main.xml adındaki layout dosyasının öğelerine erişimi için kullanılır.
 
-    private lateinit var malzemelerArrayList:ArrayList<Malzemeler>
-    val anasayfaFragment = AnasayfaFragment()
-    private lateinit var adapter: AnasayfaFragment.MalzemelerAdapter
-
     override fun onCreate(savedInstanceState: Bundle?){ // bu metot activity ilk oluştugunda çagrılır.
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater) //ilk olarak binding kullanılarak inflate edilir. Binding özelligine atanır
         setContentView(binding.root)
 
-
-
-        // MainActivity içinde MyFragment'ı çağırma
-        anasayfaFragment.setMainActivityReference(this)
-        // AnasayfaFragment'a MainActivity referansını ileterek
-        // malzemelerAdapter sınıfına erişim sağlayabilirsiniz
-
-
-        // Fragment'ı ekran üzerine yerleştirme (örneğin, bir transaction kullanarak)
-        supportFragmentManager.beginTransaction().replace(R.id.constraintLayout, anasayfaFragment).commit() // R.id.fragmentContainer, fragmentın ekran üzerinde yer alacağı container'ın id'sidir.
-
-      ////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
         val navfragment =
             supportFragmentManager.findFragmentById(R.id.navfragment) as NavHostFragment //activitenin içindeki NavHostFragment ı bulmak için kullanılr.
-                                                                      //"as NavHostFragment" bu ID li fragmenti NavHostFragment tipine dönüştürür.
-            NavigationUI.setupWithNavController(binding.navView, navfragment.navController) //Bu satır, NavigationView'ı bir NavController ile bağlamak için kullanılır.
-            // Yukarıdaki bu metot, NavigationView'ı bir NavController ile bağlamak ve gezinme işlemlerini bu bağlam üzerinden yönetmek için kullanılır.
-            // Bu sayede, NavigationView'daki menü öğeleri tıklanınca doğru fragmentlere geçiş yapılabilir.
+        //"as NavHostFragment" bu ID li fragmenti NavHostFragment tipine dönüştürür.
+        NavigationUI.setupWithNavController(binding.navView, navfragment.navController) //Bu satır, NavigationView'ı bir NavController ile bağlamak için kullanılır.
+        // Yukarıdaki bu metot, NavigationView'ı bir NavController ile bağlamak ve gezinme işlemlerini bu bağlam üzerinden yönetmek için kullanılır.
+        // Bu sayede, NavigationView'daki menü öğeleri tıklanınca doğru fragmentlere geçiş yapılabilir.
 
-            val baslik = binding.navView.inflateHeaderView(R.layout.navigation_baslik) //navView içinde R klasörü altında layout klasörü içindeki navigation_baslik isimli xml dosyasına erişti.
-            val textViewBaslik = baslik.findViewById(R.id.textViewBaslik) as TextView //R.layout.navigation_baslik bu klasör içindeki textViewBaslik adındaki textView e erişti
-            textViewBaslik.text = "Malzeme Türleri" //yazıyı belirledi
-            val ImageViewLogo = baslik.findViewById(R.id.imageViewLogo) as ImageView //R.layout.navigation_baslik bu klasör içindeki imageViewLogo adındaki ImageView e erişti
-            ImageViewLogo.setImageDrawable(resources.getDrawable(R.drawable.hsmlogo, null)) // erişilen resmi ekledi
+        val baslik = binding.navView.inflateHeaderView(R.layout.navigation_baslik) //navView içinde R klasörü altında layout klasörü içindeki navigation_baslik isimli xml dosyasına erişti.
+        val textViewBaslik = baslik.findViewById(R.id.textViewBaslik) as TextView //R.layout.navigation_baslik bu klasör içindeki textViewBaslik adındaki textView e erişti
+        textViewBaslik.text = "Malzeme Türleri" //yazıyı belirledi
+        val ImageViewLogo = baslik.findViewById(R.id.imageViewLogo) as ImageView //R.layout.navigation_baslik bu klasör içindeki imageViewLogo adındaki ImageView e erişti
+        ImageViewLogo.setImageDrawable(resources.getDrawable(R.drawable.hsmlogo, null)) // erişilen resmi ekledi
 
         binding.toolbar.title = ("Ağırlık Hesaplama") //toolbar ın başlıgı
 
 
-
-       //Navigation Drawer'ın (açılır menü) görünümünü, davranışını ve senkronizasyonunu yapılandırmak için kullanılır.
+        //Navigation Drawer'ın (açılır menü) görünümünü, davranışını ve senkronizasyonunu yapılandırmak için kullanılır.
         val toggle = ActionBarDrawerToggle(this, binding.drawer, binding.toolbar, 0, 0)
         binding.drawer.addDrawerListener(toggle)
         toggle.syncState()
 
 
-
-////////////////////////////////////////////////////////////////////
-
-
-
-
-
-        val tasarim = layoutInflater.inflate(R.layout.recycview, null)
-        val rv = tasarim.findViewById(R.id.rv) as RecyclerView
-        rv.setHasFixedSize(true)
-            rv.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL) //yanyana 2 resim olsun, dikey olsun
-
-            val m1 = Malzemeler(1,"Mil","yuvarlak_demir")
-            val m2 = Malzemeler(2,"Profil","profil")
-            val m3 = Malzemeler(1,"Boru","boru")
-
-        malzemelerArrayList = ArrayList<Malzemeler>()
-        malzemelerArrayList.add(m1)
-        malzemelerArrayList.add(m2)
-        malzemelerArrayList.add(m3)
-
-        adapter =AnasayfaFragment.MalzemelerAdapter(this,malzemelerArrayList)
-        rv.adapter = adapter
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
         binding.buttonMode.setOnClickListener {
 
             if (delegate.localNightMode== MODE_NIGHT_YES) {
@@ -142,7 +56,6 @@ class MainActivity : AppCompatActivity(){ //MainActivity Sınıfı: Bu sınıf, 
             }
         }
     }
-
         override fun onBackPressed() { //telefondaki geri tusuna basıldıgında yapılacak işlemler
             super.onBackPressed()
 
--- app/src/main/java/com/seyma/agirlikhesap_nav/Malzemeler.kt
+++
@@ -1,5 +0,0 @@
-package com.seyma.agirlikhesap_nav
-
-data class Malzemeler (var malzeme_id: Int, var malzeme_ad: String, var malzeme_resim:String) {
-
-}
